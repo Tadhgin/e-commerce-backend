e9ffbf8a161117f1022b101571d1a3ce4712fd04
Tadhgin_White <teddy_bear_morbid@hotmail.com>
2023-04-04 23:00:29 +1000

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,11 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('ecommerce_db', 'root', 'Javixavi10!', {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
-        decimalNumbers: true,
+      decimalNumbers: true,
       },
     });
 
